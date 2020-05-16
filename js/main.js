b42dac3970330f066341869ceacb426a47bcdf17
natan22g <natsuaz27@gmail.com>
2020-05-16 16:14:56 -0300

    Adicionando estilo css, organizando o head da pagina e comandos console.log

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 var nome = "Natan Castro"
 var idade = 14
+var idade2 = 20
 
-alert("meu primeiro js")
-alert("Bem vindo " + nome)
-alert("Voçê tem " + idade)
+//alert("meu primeiro js")
+//alert("Bem vindo " + nome)
+//alert("Voçê tem " + idade)
+console.log(nome)
+console.log(idade)
+console.log(idade+idade2)
