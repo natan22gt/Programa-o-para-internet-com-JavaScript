533883f0d6c9701ae058f52b8b28063518f445ca
natan22g <natsuaz27@gmail.com>
2020-05-17 16:17:55 -0300

    terminando a primeira aula de JS

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
-var nome = "Natan Castro"
-var idade = 14
-var idade2 = 20
 
+
+
+
+
+//var nome = "Natan Castro"
+//var n1 = 8
+//var n2 = 5
+//var frase = "Japão é o melhor time do mundo"
 //alert("meu primeiro js")
 //alert("Bem vindo " + nome)
 //alert("Voçê tem " + idade)
-console.log(nome)
-console.log(idade)
-console.log(idade+idade2)
+//console.log(nome)
+//console.log(n1 * n2)
+//console.log(frase.replace("Japão", "Brasil"))
+//console.log(frase.toUpperCase())
+//console.log(frase.toLowerCase())
