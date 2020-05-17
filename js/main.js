fb7d76fbff6b03dc3a6a8a67dccc6c7cb9092dcd
natan22g <natsuaz27@gmail.com>
2020-05-17 17:05:52 -0300

    aula 02: manipulação de de variaveis e tipos de variaveis

--- js/main.js
+++ js/main.js
@@ -1,8 +1,4 @@
-
-
-
-
-
+/*<aula-01>
 //var nome = "Natan Castro"
 //var n1 = 8
 //var n2 = 5
@@ -15,3 +11,21 @@
 //console.log(frase.replace("Japão", "Brasil"))
 //console.log(frase.toUpperCase())
 //console.log(frase.toLowerCase())
+*/
+
+/*<aula-02>
+//var frutas = [{nome: "maçã", cor: "vermelho"}, {nome: "uva", cor: "roxo"}, {nome: "abacate", cor: "verde"}]
+//console.log(frutas)
+//console.log(frutas[2].nome)
+//var fruta = {nome: "maçã", cor: "vermelho"}
+//console.log(fruta.cor)
+//var lista = ["maçã", "pêra", "laranja"]
+//lista.push("uva")
+//lista.pop()
+//console.log(lista)
+//console.log(lista.toString())
+//console.log(lista.join("/ /"))
+//console.log(lista.length)
+//console.log(lista.reverse())
+*/
+
